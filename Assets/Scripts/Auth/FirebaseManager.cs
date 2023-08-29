23603f25be65380d60e198ca8a24729a4ff3e97b
rodblk <38361360+rodblk@users.noreply.github.com>
2023-08-28 22:47:08 -0400

    google login

---
+++ Assets/Scripts/Auth/FirebaseManager.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Firebase.Auth;
+using Firebase.Extensions;
+using Google;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirebaseManager : MonoBehaviour
+{
+    public string GoogleWebAPI = "266029706545-1ef8qeap8kks6htjjrbh5oe0rd5q29gl.apps.googleusercontent.com";
+
+    private GoogleSignInConfiguration configuration;
+
+    private Firebase.DependencyStatus dependencyStatus = Firebase.DependencyStatus.UnavailableOther;
+    private Firebase.Auth.FirebaseAuth auth;
+    private Firebase.Auth.FirebaseUser user;
+
+    public TextMeshProUGUI UserNameTxt, UserEmailTxt;
+    public Image UserProfilePic;
+    public string imageUrl;
+    public GameObject LoginScreen, ProfileScreen;
+
+    private void Awake()
+    {
+        // Configure webAPI key with google
+        configuration = new GoogleSignInConfiguration
+        {
+            WebClientId = GoogleWebAPI,
+            RequestIdToken = true
+        };
+    }
+
+    private void Start()
+    {
+        InitFirebase();
+    }
+
+    void InitFirebase()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+    }
+
+    void GoogleSignInClick()
+    {
+        GoogleSignIn.Configuration = configuration;
+        GoogleSignIn.Configuration.UseGameSignIn = false;
+        GoogleSignIn.Configuration.RequestIdToken = true;
+        GoogleSignIn.Configuration.RequestEmail = true;
+
+        GoogleSignIn.DefaultInstance.SignIn().ContinueWith(OnGoogleAuthenticateFinished);
+    }
+
+    void OnGoogleAuthenticateFinished(Task<GoogleSignInUser> task)
+    {
+        if (task.IsFaulted)
+        {
+            Debug.LogError("Fault");
+        }
+        else if (task.IsCanceled)
+        {
+            Debug.LogError("Login Cancel");
+        }
+        else
+        {
+            Firebase.Auth.Credential credential =
+                Firebase.Auth.GoogleAuthProvider.GetCredential(task.Result.IdToken, null);
+
+            auth.SignInWithCredentialAsync(credential).ContinueWithOnMainThread(task =>
+            {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("SignInWithCredential was canceled.");
+                    return;
+                }
+
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("SignInCredential encountered an error" + task.Exception);
+                    return;
+                }
+
+                user = auth.CurrentUser;
+
+                UserNameTxt.text = user.DisplayName;
+                UserEmailTxt.text = user.Email;
+
+                LoginScreen.SetActive(false);
+                ProfileScreen.SetActive(true);
+
+                StartCoroutine(LoadImage(CheckImageUrl(user.PhotoUrl.ToString())));
+            });
+        }
+    }
+
+    private string CheckImageUrl(string url)
+    {
+        if (!string.IsNullOrEmpty(url))
+        {
+            return url;
+        }
+
+        return imageUrl;
+    }
+
+    IEnumerator LoadImage(string imageUrl)
+    {
+        WWW www = new WWW(imageUrl);
+        yield return www;
+        
+        UserProfilePic.sprite = Sprite.Create(www.texture, new Rect(0, 0, 
+            www.texture.width, www.texture.height), new Vector2(0, 0));
+    }
+}
